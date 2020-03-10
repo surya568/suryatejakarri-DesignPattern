85f68034ec956e9a23ac007417c44dd9d4a83e75
surya568 <17pa1a0568@vishnu.edu.in>
2020-03-10 16:40:17 +0530

    commit

---
+++ design_pattern/src/main/java/com/surya/design_pattern/App.java
@@ -0,0 +1,22 @@
+package com.surya.design_pattern;
+import AdapterPattern.*;
+import BridgePattern.*;
+import CommandPattern.*;
+import PrototypePattern.*;
+import TemplatePattern.*;
+import SingletonPattern.*;
+
+public class App 
+{
+    @SuppressWarnings("unused")
+	public static void main( String[] args )
+    {
+        DriverAdapter ap = new DriverAdapter();
+        DriverBridge bp = new DriverBridge();
+        DriverCommand cp = new DriverCommand();
+        DriverPrototype ip = new DriverPrototype();
+        DriverSingleton fp = new DriverSingleton();
+        DriverTemplate afp = new DriverTemplate();
+        
+    }
+}
